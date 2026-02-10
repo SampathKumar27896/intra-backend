209edcd57e70f61be763cac57b63ec11b7937fad
Sampath Kumar P N <sampathkumar27896@hotmail.com>
2026-02-11 00:53:59 +0530

    Extended git session

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -42,7 +42,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
-        maxAge: 60000,
+        maxAge: 60 * 60 * 1000,
       });
       return {
         message: "Login success",
